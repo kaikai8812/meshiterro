cd6db76500154bc02e8225cca8999bea04304eb1
kaikai8812 <sauna.kaikai@gmail.com>
2021-01-06 08:00:12 +0000

    投稿関係

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'b30668f7c30540bd5ef604a0fdbb92081a3fe1cd6a9367110be3dfe1859a9ce866cefb6160fbe849c0de5d1673cbf04a8d1b189ca0b8972cc5fb77f361c8099f'
