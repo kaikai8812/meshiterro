19bb980a44303b2454f787b884d3975917f7432d
kaikai8812 <sauna.kaikai@gmail.com>
2021-01-07 06:21:52 +0000

    マイページまで作成

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,4 +1,13 @@
 class PostImage < ApplicationRecord
   belongs_to :user
   attachment :image
+  has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
+  # userモデルのidと、favoritesモデルのuser_idが同じであれば、trueを返すメソッド →違うかも
+  # ここでのuser引数は、単なる定義であり、userモデルのというわけではない？
+  
 end
